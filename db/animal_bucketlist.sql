4b17265abf837f590797199859c632b311e51b49
Kelly Rawstron <code.kelly8@gmail.com>
2022-02-05 17:07:29 +0000

    created db and run sql, tested. created animal and type classes

---
+++ db/animal_bucketlist.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    animal_name VARCHAR(255)
+);
+
+CREATE TABLE types (
+    id SERIAL PRIMARY KEY,
+    animal_type VARCHAR(255),
+    type_seen BOOLEAN,
+    animal_id INT REFERENCES animals(id)
+);
---
+++ db/models/type.py
@@ -0,0 +1,7 @@
+class Type:
+    
+    def __init__(self, animal_type, animal, type_seen = False, id = None):
+        self.animal_type = animal_type
+        self.animal = animal
+        self.type_seen = type_seen
+        self.id = id
