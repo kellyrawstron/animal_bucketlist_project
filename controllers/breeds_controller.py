bfdee3aaa9e217c0ea2de31b0d26c12d150e5b4a
Kelly Rawstron <code.kelly8@gmail.com>
2022-02-06 17:57:37 +0000

    it is working, needed to close and start again... added in some animal values and posted and redirected to animal page, put values to show in breeds/index.html to see if was working and it is showing on page

--- controllers/breeds_controller.py
+++ controllers/breeds_controller.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, Blueprint, request, redirect
+from models.breed import Breed
 import repositories.breed_repository as breed_repository
 import repositories.animal_repository as animal_repository
-from models.breed import Breed
+
 
 
 breeds_blueprint = Blueprint("breeds", __name__)
@@ -15,3 +16,13 @@ def breeds():
 def new_breed():
     animals = animal_repository.select_all()
     return render_template('breeds/new.html', animals = animals)
+
+@breeds_blueprint.route("/breeds", methods=['POST'])
+def create_breed():
+    breed_kind = request.form['breed_kind']
+    animal_id = request.form['animal_id']
+    breed_seen = request.form['breed_seen']
+    animal = animal_repository.select(animal_id)
+    breed = Breed(breed_kind, animal, breed_seen)
+    breed_repository.save(breed)
+    return redirect('/breeds')
